2f31fa170048f7702f9e80f4abaa7e8b69ce3d56
Emmanuel P. Dela Peña <168615722+Tis-Emman@users.noreply.github.com>
2024-06-22 00:39:17 +0800

    Update constructors.cpp

--- constructors.cpp
+++ constructors.cpp
@@ -23,14 +23,40 @@ class Student{
 
 
 
+
+
 };
+
+class Construction_Worker{
+
+    public:
+
+    std::string name;
+    int age;
+    std::string place;
+    std::string company;
+
+    Construction_Worker(std::string name, int age, std::string place, std::string company){ //FUNCTION WITH PARAMETER
+    this->name = name;
+    this->age = age;
+    this->company = company;
+    this->place = place;
+
+
+    };
+};
+
+
 
 
 int main(){
 
+    Construction_Worker worker("PETER", 18, "Baliuag construction market", "EMEREN"); //This calls the function and ssign its arguments to call the paramete
+        std::cout << worker.name  << '\n' << worker.age << '\n' << worker.company << '\n' << worker.place << '\n' << '\n'; //To print 
+        
     
 
-    Student student("Ronald", 17, 92.7);
+    Student student("STEVE", 17, 92.7);
         std::cout << student.name << '\n' << student.GWA << '\n'  << student.age << '\n' ;
 
 
